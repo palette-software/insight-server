f99d7c9c977c0d39ff3015d6275da83e7ff38a83
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-03-01 15:54:07 +0100

    small refactorings towards a better upload mechanism

--- app/models/upload.go
+++ app/models/upload.go
@@ -3,8 +3,10 @@ package models
 import (
 	"github.com/revel/revel"
 
+	"crypto/md5"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -25,7 +27,7 @@ type UploadedFile struct {
 	UploadedPath string
 
 	// The md5 of the file
-	Md5 string
+	Md5 []byte
 }
 
 // Represents an uploaded CSV file with its metadata
@@ -67,49 +69,86 @@ func getUploadPathForFile(filename, fileHash string, requestTime time.Time) stri
 	return filepath.ToSlash(path.Join(folderTimestamp, fullFileName))
 }
 
-///// Returns the path where a file needs to be placed
-//func getUploadPath(uploadBasePath, filename string, requestTime time.Time, fileHash string) string {
-//// the folder name is only the date
-//folderTimestamp := requestTime.Format("2006-01-02")
-//// the file name gets the timestamp appended (only time)
-//fileTimestamp := requestTime.Format("15-04--05-00")
-
-//// get the extension and basename
-//fileBaseName := SanitizeName(filename)
-//fileExtName := SanitizeName(path.Ext(filename))
-//fullFileName := fmt.Sprintf("%v-%v-%v.%v", fileBaseName, fileTimestamp, fileHash, fileExtName[1:])
-
-//// the file name is the sanitized file name
-//return filepath.ToSlash(path.Join(uploadBasePath, folderTimestamp, fullFileName))
-//}
-
 // tries to create a new uploaded file. This method does not check the md5, just uses it as part of the filename
-func NewUploadedFile(uploadBasePath, filename, md5 string, requestTime time.Time, reader io.Reader) (*UploadedFile, error) {
-	//outputPath := getUploadPath(tenant.HomeDirectory, pkg, filename, requestTime, md5)
-	outputPath := filepath.ToSlash(path.Join(uploadBasePath, getUploadPathForFile(filename, md5, requestTime)))
+func NewUploadedFile(uploadBasePath, filename string, requestTime time.Time, reader io.Reader) (*UploadedFile, error) {
 
-	// create the directory of the file
+	hash := md5.New()
+	// create a TeeReader that automatically forwards bytes read from the file to
+	// the md5 hasher's reader
+	readerWithMd5 := io.TeeReader(reader, hash)
+
+	// create a temp file to move the bytes to (we do not yet know the hash of the file)
+	tmpFile, err := ioutil.TempFile("", "temporary-file-contents")
+	if err != nil {
+		return nil, err
+	}
+	defer tmpFile.Close()
+
+	// write the data to the temp file (and hash in the meantime)
+	bytesWritten, err := io.Copy(tmpFile, readerWithMd5)
+	if err != nil {
+		return nil, err
+	}
+	revel.INFO.Printf("[Upload] written %v bytes to '%v'", bytesWritten, tmpFile.Name())
+
+	// get the hash from the teewriter
+	fileHash := hash.Sum(nil)
+	// make a hex string out of the md5
+	md5str := fmt.Sprintf("%x", fileHash)
+
+	// generate the output file name
+	outputPath := filepath.ToSlash(path.Join(uploadBasePath, getUploadPathForFile(filename, md5str, requestTime)))
+
+	// create the output file path
 	if err := os.MkdirAll(filepath.Dir(outputPath), OUTPUT_DEFAULT_DIRMODE); err != nil {
 		return nil, err
 	}
 
-	// Create the output file
-	newFile, err := os.Create(outputPath)
-	if err != nil {
-		return nil, err
-	}
-	defer newFile.Close()
+	// Get the temp file name before closing it
+	tempFilePath := tmpFile.Name()
 
-	// Copy the bytes to destination from source
-	bytesWritten, err := io.Copy(newFile, reader)
+	// close the temp file, so writes get flushed
+	tmpFile.Close()
+
+	// move the output file to the new path with the new name
+	err = os.Rename(tempFilePath, outputPath)
 	if err != nil {
 		return nil, err
 	}
-	revel.INFO.Printf("Copied %d bytes to '%v'", bytesWritten, outputPath)
+
+	revel.INFO.Printf("[Upload] Moved '%v' to '%v'", tempFilePath, outputPath)
 
 	return &UploadedFile{
 		Filename:     filename,
 		UploadedPath: outputPath,
-		Md5:          md5,
+		Md5:          fileHash,
+	}, nil
+}
+
+// Create a new UploadedCsv struct from the provided parameters.
+// As io.Reader provides no way of cloning the reader itself,
+// we need to pass both file and metadata readers twice so we can
+// use it for both copying and hashing
+func NewUploadedCsv(tenant *Tenant, pkg, filename, filemd5 string, requestTime time.Time, fileReader, metadataReader io.Reader) (*UploadedCsv, error) {
+
+	// get the base path for uploads
+	basePath := getUploadBasePath(tenant.HomeDirectory, pkg)
+
+	mainFile, err := NewUploadedFile(basePath, filename, requestTime, fileReader)
+	if err != nil {
+		return nil, err
+	}
+
+	metaFile, err := NewUploadedFile(basePath, fmt.Sprintf("%s.meta", filename), requestTime, metadataReader)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UploadedCsv{
+		Csv:      *mainFile,
+		Metadata: *metaFile,
+		Uploader: *tenant,
+		Package:  pkg,
+		HasMeta:  true,
 	}, nil
 }
--- tests/uploaded_filetest.go
+++ tests/uploaded_filetest.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/palette-software/insight-server/app/models"
 
+	"bytes"
+	"crypto/md5"
 	"fmt"
 	"path"
 	"path/filepath"
@@ -17,19 +19,26 @@ type UploadedFileTest struct {
 }
 
 func (t *UploadedFileTest) TestSingleUploadedFile() {
-	testReader := strings.NewReader("Hello world")
+	testContent := "Hello world"
+	testReader := strings.NewReader(testContent)
 	filename := "test.txt"
-	md5 := "ABC"
+
+	// we need to do some stupid conversion from [16]byte to []byte here or
+	// bytes.Equal will fail with argumenterror (fixed array vs slice)
+	// reflect.DeepEqual will fail (different types dont match)
+	fileMd5a := md5.Sum([]byte(testContent))
+	fileMd5 := fileMd5a[0:]
+
 	basePath := models.GetOutputDirectory()
 	reqTime := time.Date(2016, 03, 15, 12, 00, 00, 00, time.UTC)
 
-	uploadedFile, err := models.NewUploadedFile(basePath, filename, md5, reqTime, testReader)
+	uploadedFile, err := models.NewUploadedFile(basePath, filename, reqTime, testReader)
 	t.Assert(err == nil)
 
-	expectedFileName := fmt.Sprintf("test-txt-12-00--00-00-%v.txt", md5)
+	expectedFileName := fmt.Sprintf("test-txt-12-00--00-00-%x.txt", fileMd5)
 	expectedUploadPath := filepath.ToSlash(path.Join(basePath, "2016-03-15", expectedFileName))
 
 	t.Assert(uploadedFile.Filename == filename)
 	t.Assert(uploadedFile.UploadedPath == expectedUploadPath)
-	t.Assert(uploadedFile.Md5 == md5)
+	t.Assert(bytes.Equal(uploadedFile.Md5, fileMd5))
 }
