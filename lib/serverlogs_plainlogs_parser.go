a8573f5a4dd1bf92613a36a97b811421959ecfb6
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-07-05 11:48:53 +0200

    remove tde_server continuation feature

--- lib/serverlogs_plainlogs_parser.go
+++ lib/serverlogs_plainlogs_parser.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
-
-	"github.com/Sirupsen/logrus"
 )
 
 // Plain logs
@@ -19,26 +17,15 @@ var (
 const (
 	plainServerlogsTimestampFormat = "2006-01-02 15:04:05.999"
 	jsonDateFormat                 = "2006-01-02T15:04:05.999"
-
-	pidHeaderKey = "pid-header"
 )
 
 // The key of the pid header in the parser state
 
 type PlainLogParser struct {
-	// The DB housing our continuations
-	ContinuationDb LogContinuation
 }
 
 func MakePlainlogParser(dbDirectory string) (*PlainLogParser, error) {
-	logDb, err := MakeBoltDbLogContinuationDb(dbDirectory)
-	if err != nil {
-		return nil, fmt.Errorf("Cannot open log continuation db: '%v'", err)
-	}
-
-	return &PlainLogParser{
-		ContinuationDb: logDb,
-	}, nil
+	return &PlainLogParser{}, nil
 }
 
 // Headers for the plain serverlog files
@@ -65,12 +52,12 @@ func (p *PlainLogParser) Parse(state ServerlogParserState, src *ServerlogsSource
 	// parse the timestamp
 	tsUtc, err := convertTimestringToUTC(plainServerlogsTimestampFormat, ts, src.Timezone)
 	if err != nil {
-		return fmt.Errorf("Parsing log timestamp: %v", err)
+		return fmt.Errorf("Error parsing log timestamp: %v", err)
 	}
 
 	// parse the pid (so we can check if is a valid number)
 	if _, err := strconv.Atoi(pid); err != nil {
-		return fmt.Errorf("Parsing pid '%s': %v", pid, err)
+		return fmt.Errorf("Error parsing pid '%s': %v", pid, err)
 	}
 
 	// ==================== Elapsed ====================
@@ -84,88 +71,6 @@ func (p *PlainLogParser) Parse(state ServerlogParserState, src *ServerlogsSource
 	} else {
 		elapsed = "0"
 		start_ts = tsUtc
-	}
-
-	// ==================== Continuations ====================
-
-	switch {
-	// check if this line looks like a continuation
-	case IsLineContinuation(line):
-		// Create the continuation key from the line contents
-		continuationKey := MakeContinuationKey(src.Host, tsUtc, pid)
-
-		//check if we have a header for this continuation key
-		pidHeader, hasPidHeader, err := p.ContinuationDb.HeaderLineFor(continuationKey)
-
-		// handle errors
-		if err != nil {
-			logrus.WithError(err).WithFields(logrus.Fields{
-				"component": "serverlogs",
-				"key":       string(continuationKey),
-			}).Error("Error getting PID header from LogContinuationDb")
-		}
-
-		// handle good things (we have the pid header)
-		if hasPidHeader {
-			// if we have, emit it before the current line, and
-			// use pidHeader instead of line
-			w.WriteParsed(src, []string{tsUtc, pid, string(pidHeader), elapsed, start_ts})
-			// Store the looked up pid header for the file
-			// (so we can save it when the log file is rotated
-			state.Set(pidHeaderKey, pidHeader)
-			logrus.WithFields(logrus.Fields{
-				"component": "serverlogs",
-				"line":      line,
-				"pidHeader": pidHeader,
-			}).Info("Emitted pid header for continuation, updated continuation pid in state")
-		} else {
-
-			// Log the fact that we could not find the continuation key.
-			// TODO: should this be 'WARN' or 'INFO'?
-			logrus.WithFields(logrus.Fields{
-				"component": "serverlogs",
-				"line":      line,
-				"key":       string(continuationKey),
-			}).Warn("Cannot find pid header for TDE log continuation")
-		}
-
-	// Check if this line looks like a log file end
-	case LineWillHaveContinuation(line):
-		continuationKey := MakeContinuationKey(src.Host, tsUtc, pid)
-
-		// Try to get the pid header from the state
-		if pidHeader, hasPidHeader := state.Get(pidHeaderKey); hasPidHeader {
-			// if we have the pid header, store it in the DB for this
-			// continuation key
-			err := p.ContinuationDb.SetHeaderFor(continuationKey, pidHeader)
-			if err != nil {
-				// Log any errors
-				logrus.WithError(err).WithFields(logrus.Fields{
-					"component":       "serverlogs",
-					"line":            line,
-					"continuationKey": continuationKey,
-				}).Error("Error getting pid header from TDE log continuation DB")
-			} else {
-				// Log the fact we are storing the pid here
-				logrus.WithFields(logrus.Fields{
-					"component":       "serverlogs",
-					"line":            line,
-					"continuationKey": continuationKey,
-					"pidHeader":       pidHeader,
-				}).Info("Storing TDE server continuation pid in DB")
-			}
-
-		}
-
-	// Check if this line is a pid header
-	case LineHasPid(line):
-		// store the current line as the pid header in this case
-		state.Set(pidHeaderKey, []byte(line))
-
-		logrus.WithFields(logrus.Fields{
-			"component": "serverlogs",
-			"line":      line,
-		}).Info("Saving TDE server pid header in state")
 	}
 
 	// ==================== Emitting the line ====================
